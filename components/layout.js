d0456a1af88d57e95d6a5fa703df119ac3f5a24b
Christen <christen.bc@gmail.com>
2023-10-26 00:09:54 +0200

    finish basic app

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'Christen Blom-Dahl';
+export const siteTitle = 'Next.js Personal Website';
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="A personal website using Next.js"
         />
         <meta
           property="og:image"
