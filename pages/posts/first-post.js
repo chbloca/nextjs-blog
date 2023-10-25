7ba1eb149f0dc63688c1c245e0a88fb59da5a6f1
Christen <christen.bc@gmail.com>
2023-10-25 19:58:33 +0200

    create component and style it

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,10 +2,11 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -26,6 +27,6 @@ export default function FirstPost() {
                 width={144} // Desired size with correct aspect ratio
                 alt="Your Name"
             />
-        </>
+        </Layout>
     );
 }
