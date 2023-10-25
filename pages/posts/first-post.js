f02e2067774714250b6c9cb8a120c64240efe9aa
Christen <christen.bc@gmail.com>
2023-10-25 20:14:08 +0200

    apply style and draft base content

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -10,23 +10,10 @@ export default function FirstPost() {
             <Head>
                 <title>First Post</title>
             </Head>
-            <Script
-                src="https://connect.facebook.net/en_US/sdk.js"
-                strategy="lazyOnload"
-                onLoad={() =>
-                    console.log(`script loaded correctly, window.FB has been populated`)
-                }
-            />
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-            <Image   
-                src="/images/profile.jpg" // Route of the image file
-                height={144} // Desired size with correct aspect ratio
-                width={144} // Desired size with correct aspect ratio
-                alt="Your Name"
-            />
         </Layout>
     );
 }
