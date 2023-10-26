e48b1b7dc8fbb19717477f63bb3444460082c31e
Christen <christen.bc@gmail.com>
2023-10-26 02:29:54 +0200

    implement capacitor

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    /* config options here */
+    images: {
+        unoptimized: true,
+    },
+  }
+   
+module.exports = nextConfig
